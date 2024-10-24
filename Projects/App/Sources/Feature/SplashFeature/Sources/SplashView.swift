6cd4d3d1047b8bf858c712d44a6e2cb5da0035ba
“xixn2” <ssbkh0802@naver.com>
2024-10-24 18:20:00 +0900

    💄 :: [#29] Add Expo Main Logo file

---
+++ Projects/App/Sources/Feature/SplashFeature/Sources/SplashView.swift
@@ -0,0 +1,19 @@
+//
+//  SplashView.swift
+//  Expo-iOS
+//
+//  Created by 서지완 on 10/24/24.
+//  Copyright © 2024 SchoolofCompany. All rights reserved.
+//
+
+import SwiftUI
+
+struct SplashView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SplashView()
+}
